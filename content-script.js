8311c65bbe2155fcb47bcded1dce7fcbfd8ec0cf
Ben Alpert <ben@benalpert.com>
2015-01-11 10:38:16 -1000

    Poll for React's presence every second

--- content-script.js
+++ content-script.js
@@ -1,4 +1,12 @@
-var runningReact = !!document.querySelector('[data-reactid]');
-if (runningReact) {
-  chrome.runtime.sendMessage({react: true});
+function checkForReact() {
+  var runningReact = !!document.querySelector('[data-reactid]');
+  if (runningReact) {
+    chrome.runtime.sendMessage({react: true});
+  } else {
+    // React isn't currently present, check again in a second in case it's
+    // loaded after the page is initialized.
+    setTimeout(checkForReact, 1000);
+  }
 }
+
+checkForReact();
